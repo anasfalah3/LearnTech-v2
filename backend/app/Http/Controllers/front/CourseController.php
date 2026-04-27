7358e72b32c92de63c264eab603562eac9b39938
anasfalah3 <anasfalah3@gmail.com>
2026-04-27 14:41:55 +0100

    Backend - Cover Image API

--- backend/app/Http/Controllers/front/CourseController.php
+++ backend/app/Http/Controllers/front/CourseController.php
@@ -8,7 +8,10 @@ use App\Models\Course;
 use App\Models\Language;
 use App\Models\Level;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class CourseController extends Controller
 {
@@ -112,6 +115,60 @@ class CourseController extends Controller
             'status' => 200,
             'data' => $course,
             'message' => 'Course updated successfully'
+        ], 200);
+    }
+
+    public function saveCourseImage($id, Request $request)
+    {
+        $course = Course::find($id);
+        if ($course == null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Course not found'
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+        if ($course->image != "") {
+            if (File::exists(public_path('uploads/course/' . $course->image))) {
+                File::delete(public_path('uploads/course/' . $course->image));
+            }
+            if (File::exists(public_path('uploads/course/small/' . $course->image))) {
+                File::delete(public_path('uploads/course/small/' . $course->image));
+            }
+        }
+
+        $image = $request->image;
+        $ext = $image->getClientOriginalExtension();
+        $imageName = strtotime('now') . '-' . $id . '.' . $ext;
+
+        $image->move(public_path('uploads/course'), $imageName);
+
+        // Create Small Thumbnail
+        $manager = ImageManager::usingDriver(Driver::class);
+        $img = $manager->decode(public_path('uploads/course/' . $imageName));
+
+        // crop the best fitting 5:3 (600x360) ratio and resize to 600x360 pixel
+        $img->cover(750, 450);
+
+        $img->save(public_path('uploads/course/small/' . $imageName));
+
+
+        $course->image = $imageName;
+        $course->save();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $course,
+            'message' => 'Image uploaded successfully'
         ], 200);
     }
 }
